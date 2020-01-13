1b7254da0322c260206f810573c133ab1df547af
github username <lluisguardiola.h@gmail.com>
2020-01-13 16:44:10 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(arr, str){
+    return arr.filter(n => n.toLowerCase() === str.toLowerCase())
+}
+
+function fuzzyMatch(arr, str){
+
+    return arr.filter(n => {
+        return n.startsWith(str)
+    })
+}
+
+function matchName(arr, str){
+    return arr.filter(n => n.name.toLowerCase() === str.toLowerCase())
+}
